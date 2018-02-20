ee7101dae24bcb38071ea1bb1f50df8408220347
Brandon Whisnant <bc.whisnant@gmail.com>
2018-02-20 13:16:17 -0500

    further optimized template literals to include social section

--- js/layout.js
+++ js/layout.js
@@ -9,5 +9,16 @@ let pageContent = `
     <p>${companySubParagraph}</p>
 `;
 
+let socialContent = `
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/brandon-c-whisnant" target="_blank"><i class="fab fa-linkedin-in fa-4x"></i></a></li>
+                    <li><a href="https://www.github.com/bc-whisnant" target="_blank"><i class="fab fa-github fa-4x"></i></a></li>
+                    <li><a href="mailto:bc.whisnant@gmail.com"><i class="far fa-envelope fa-4x"></i></a></li>
+                </ul>
+            ;`
+
 let titleStatement = document.getElementById('title');
 titleStatement.innerHTML = pageContent;
+
+let socialSection = document.getElementById('social-content');
+socialSection.innerHTML = socialContent;
